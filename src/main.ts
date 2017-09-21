7ca94183ce38235dc429b1eeedf9ba7182902e23
Quinntyne Brown <quinntynebrown@gmail.com>
2017-09-21 11:26:07 -0400

    inital

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+﻿import { HubConnection } from "@aspnet/signalr-client";
+
+let connection = new HubConnection(window["__HUB_ENDPOINT__"]);
+
+connection.send('send', data => {
+    console.log(data);
+});
+
+connection.start()
+    .then(() => connection.invoke('send', 'Hello'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+﻿const webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        'app': './src/main'
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].js",
+        publicPath: "dist/"
+    },
+    resolve: {
+        extensions: ['.ts','.css','js','.json', '.html']
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: 'raw-loader' },
+            { test: /\.html$/, loaders: ['html-loader'] },
+            { test: /\.ts$/, loaders: ['awesome-typescript-loader'], exclude: /node_modules/ }
+        ]
+    }
+};
